19f514c78d23a8c17948e2fe476e52cff3b36ba3
tsuchimore1 <yoshi1003to216@gmail.com>
2016-02-26 13:49:40 +0000

    first

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
-    haves.create(item_id: item.id)
+    haves.find_or_create_by(item_id: item.id)
   end
 
   def unhave(item)
@@ -48,7 +48,7 @@ class User < ActiveRecord::Base
   end
 
   def want(item)
-    wants.create(item_id: item.id)
+    wants.find_or_create_by(item_id: item.id)
   end
 
   def unwant(item)
