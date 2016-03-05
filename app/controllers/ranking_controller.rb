0bb4303381c12ba98d618c8db4e5a36512157f42
tsuchimore1 <yoshi1003to216@gmail.com>
2016-03-05 07:31:57 +0000

    Gemfile update for heroku

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,9 +1,12 @@
 class RankingController < ApplicationController
     def have
-      @items = Item.group(:haves)
+      @item_counts = Have.group(:item_id).order('count_item_id desc').limit(10).count('item_id').keys
+      @items = Item.find(@item_counts).sort_by {|itemc| @item_counts.index(itemc.id)}
     end
     
-    def want 
-      @items = Item.group(:wants)
-    end        
+    def want
+      @item_counts = Want.group(:item_id).order('count_item_id desc').limit(10).count('item_id').keys
+      @items = Item.find(@item_counts).sort_by {|itemc| @item_counts.index(itemc.id)}
+    end
+
 end
